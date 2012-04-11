80debe360299ce29536da3848edd588134225202
Sam Carter <sam.w.carter@gmail.com>
2012-04-11 17:21:32 +0100

    temp

--- PastTweets.rb
+++ PastTweets.rb
@@ -1,9 +1,22 @@
 require('rubygems')
-gem('twitter4r', '0.7.0')
 require('twitter')
+require('twitter/console')
 
+gem('twitter4r', '0.7.0')
 
-twitter = Twitter::Client
+Twitter::Client.configure do |conf|
+	conf.application_name = 'PastTweets'
+	conf.application_version = '0.1'
+	conf.application_url = 'http://github.com/cheesysam/PastTweets'
+
+    conf.oauth_consumer_token = 'urW1stBADHpeTLhLwd7g'
+    conf.oauth_consumer_secret = 'I9E9TgJzVF6caTXma5uCNI2yuzsfC0N5KUpgBTXrJY'
+end
+	
+cfg = Twitter::Client.config
+
+client = Twitter::Client.new(:oauth_access =>
+{  :key => '19745640-Z9yKLyw2
 
 user = twitter.user('cheesysam')
 puts user.my(:info)
