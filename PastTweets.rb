b3d7bdd8fd2bbe4370493eb16359741fe8432560
Sam Carter <sam.w.carter@gmail.com>
2012-04-12 15:08:16 +0100

    Probably final version using twitter4r lib

--- PastTweets.rb
+++ PastTweets.rb
@@ -2,6 +2,7 @@ require('rubygems')
 require('twitter')
 require('twitter/console')
 
+#gem() is like require but specifies the version
 gem('twitter4r', '0.7.0')
 
 Twitter::Client.configure do |conf|
@@ -21,4 +22,13 @@ client = Twitter::Client.new(:oauth_access =>
 })
 
 #puts client.user 'cheesysam'
-puts client.timeline_for :user, :id => 'matty_jwilliams', :count =>50
+#puts client.timeline_for(:user, :id => 'matty_jwilliams', :count =>50, :since =>startTime){|status| puts status.text}
+#puts client.timeline_for(:user, :id => 'matty_jwilliams', :since =>Time.now - 60*60){|status| puts status.text}
+#puts '-----'
+#puts client.timeline_for(:user, :id => 'matty_jwilliams', :count=>1, :since 10.weeks.ago) {|status| puts status.text}
+
+startTime = Time.now - 60*60*24*7
+
+historyTweets = client.timeline_for(:user, :id=> 'cheesysam', :count=>10, :since => Time.now) do
+    |status| puts status.user.screen_name + ": "+ status.text
+end
